416e2aa663db49f23f699881ae88d003e1b086e7
cvl95 <christophervanle1@googlemail.com>
2018-06-10 20:35:08 +0200

    Proxy sollte gehen

--- src/de/hsa/games/fatsquirrel/botapi/TestBot.java
+++ src/de/hsa/games/fatsquirrel/botapi/TestBot.java
@@ -1,12 +1,12 @@
 package de.hsa.games.fatsquirrel.botapi;
 
+
 public class TestBot implements BotController {
 
 	public TestBot() {
-		// TODO Auto-generated constructor stub
 	}
 
-	@Override
+	
 	public void nextStep(ControllerContext view) {
 		
 		//TODO: Logik für Bot einstellen
--- src/de/hsa/games/fatsquirrel/botapi/TestBotFactory.java
+++ src/de/hsa/games/fatsquirrel/botapi/TestBotFactory.java
@@ -1,16 +1,30 @@
 package de.hsa.games.fatsquirrel.botapi;
 
+import java.lang.reflect.Proxy;
+
+import de.hsa.games.fatsquirrel.proxies.BotInvocationHandler;
+
 public class TestBotFactory implements BotControllerFactory{
+   
 
 	@Override
 	public BotController createMasterBotController() {
-		return new TestBot();
+	   BotController masterBot= new TestBot();
+	   
+	   BotInvocationHandler handler = new BotInvocationHandler(masterBot);
+	   BotController masterBotProxy = (BotController) Proxy.newProxyInstance(BotController.class.getClassLoader(), new Class[] {BotController.class}, handler);
+	   
+		return masterBotProxy;
 	}
 
 	@Override
 	public BotController createMiniBotController() {
-		// TODO Auto-generated method stub
-		return new TestBot();
+	   BotController miniBot = new TestBot();
+      
+      BotInvocationHandler handler = new BotInvocationHandler(miniBot);
+      BotController miniBotProxy = (BotController) Proxy.newProxyInstance(BotController.class.getClassLoader(), new Class[] {BotController.class}, handler);
+      
+      return miniBotProxy;
 	}
 
 }
