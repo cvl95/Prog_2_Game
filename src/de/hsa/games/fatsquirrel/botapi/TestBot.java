4f30368b2b13fd02d9a53f5bdfee0a108cc1fa44
Deadmin1 <Fatih.karakuzu@t-online.de>
2018-06-11 12:25:40 +0200

    Mergeconflict behoben

--- src/de/hsa/games/fatsquirrel/botapi/TestBot.java
+++ src/de/hsa/games/fatsquirrel/botapi/TestBot.java
@@ -1,18 +1,11 @@
 package de.hsa.games.fatsquirrel.botapi;
 
-<<<<<<< HEAD
 import de.hsa.games.fatsquirrel.space.XY;
-=======
->>>>>>> 416e2aa663db49f23f699881ae88d003e1b086e7
+
 
 public class TestBot implements BotController {
 
 	public TestBot() {
-<<<<<<< HEAD
-
-	
-=======
->>>>>>> 416e2aa663db49f23f699881ae88d003e1b086e7
 	}
 
 	
--- src/de/hsa/games/fatsquirrel/botapi/TestBotFactory.java
+++ src/de/hsa/games/fatsquirrel/botapi/TestBotFactory.java
@@ -19,16 +19,14 @@ public class TestBotFactory implements BotControllerFactory{
 
 	@Override
 	public BotController createMiniBotController() {
-<<<<<<< HEAD
 		return new TestBot();
-=======
 	   BotController miniBot = new TestBot();
       
       BotInvocationHandler handler = new BotInvocationHandler(miniBot);
       BotController miniBotProxy = (BotController) Proxy.newProxyInstance(BotController.class.getClassLoader(), new Class[] {BotController.class}, handler);
       
       return miniBotProxy;
->>>>>>> 416e2aa663db49f23f699881ae88d003e1b086e7
+
 	}
 
 }
